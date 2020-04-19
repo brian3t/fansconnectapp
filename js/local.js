744e6b7dda8f476abc48badec6c44779237faf8b
Brian Nguyen <ngxtri@gmail.com>
2020-04-19 13:16:33 -0700

    Create local.js

---
+++ js/local.js
@@ -0,0 +1,15 @@
+// var IS_LOCAL = true;
+var IS_LOCAL = false;
+
+const CONFIG = {
+    restUrl: 'https://api.livenout.usvsolutions.com/v1/',
+    // date_range_day: 34,//asdf
+};
+
+var ADMINROOT = 'https://admin.livenout.usvsolutions.com/';
+
+if (IS_LOCAL){
+    CONFIG.restUrl = 'https://api.lnoapi/v1/'
+    CONFIG.date_range_day = 34
+    ADMINROOT = 'https://lnoapi/'
+}
